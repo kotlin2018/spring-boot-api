98b576f86309d5b4dfdf1512c1de6d96f8adba18
stefano <huzhichao502@gmail.com>
2022-03-25 23:18:00 +0800

    swagger

--- src/main/java/com/mltt/MlttApplication.java
+++ src/main/java/com/mltt/MlttApplication.java
@@ -4,11 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 
 
+@EnableOpenApi
 @MapperScan("com.mltt.mapper")
 @SpringBootApplication
 public class MlttApplication {
